353964063c8f8b3dff193fcc0dad6fe3f6a2795c
aniltaha <60629661+ATA10@users.noreply.github.com>
2025-12-10 20:32:44 +0300

    1

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -79,7 +79,194 @@ export const ProjectList = [
 
 // --------------------------------------- Skills
 
+// export const stackList = [
+//   {
+//     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/flutter/flutter-original.svg",
+//     name: "Flutter",
+//   },
+//   {
+//     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg",
+//     name: "HTML",
+//   },
+//   {
+//     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg",
+//     name: "CSS",
+//   },
+//   {
+//     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+//     name: "JavaScript",
+//   },
+//   {
+//     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+//     name: "ReactJS",
+//   },
+//   {
+//     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nextjs/nextjs-original.svg",
+//     name: "NextJs",
+//   },
+//   {
+//     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+//     name: "Typescript",
+//   },
+//   {
+//     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg",
+//     name: "Git",
+//   },
+//   {
+//     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+//     name: "React Native",
+//   },
+//   {
+//     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg",
+//     name: "NodeJS",
+//   },
+//   {
+//     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/firebase/firebase-plain.svg",
+//     name: "Firebase",
+//   },
+//   {
+//     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original.svg",
+//     name: "MongoDB",
+//   },
+//   {
+//     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/cassandra/cassandra-original.svg",
+//     name: "Cassandra",
+//   },
+//   {
+//     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+//     name: "Python",
+//   },
+//   {
+//     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/flask/flask-original.svg",
+//     name: "Flask",
+//   },
+//   {
+//     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+//     name: "Java",
+//   },
+//   {
+//     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original.svg",
+//     name: "MySQL",
+//   },
+//   {
+//     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original.svg",
+//     name: "Bootstrap",
+//   },
+//   {
+//     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/materialui/materialui-original.svg",
+//     name: "Material UI",
+//   },
+//   {
+//     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg",
+//     name: "C",
+//   },
+//   {
+//     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/csharp/csharp-original.svg",
+//     name: "C#",
+//   },
+//   {
+//     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg",
+//     name: "C++",
+//   },
+//   {
+//     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/matlab/matlab-original.svg",
+//     name: "MatLab",
+//   },
+//   {
+//     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/kaggle/kaggle-original.svg",
+//     name: "Kaggle",
+//   },
+//   {
+//     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/keras/keras-original.svg",
+//     name: "Keras",
+//   },
+//   {
+//     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/tensorflow/tensorflow-original.svg",
+//     name: "TensorFlow",
+//   },
+//   {
+//     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/opencv/opencv-original.svg",
+//     name: "OpenCV",
+//   },
+//   {
+//     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/pytorch/pytorch-original.svg",
+//     name: "PyTorch",
+//   },
+//   {
+//     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/scikitlearn/scikitlearn-original.svg",
+//     name: "Scikit-learn",
+//   },
+//   {
+//     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/pandas/pandas-original.svg",
+//     name: "Pandas",
+//   },
+//   {
+//     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/numpy/numpy-original.svg",
+//     name: "NumPy",
+//   },
+//   {
+//     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/arduino/arduino-original.svg",
+//     name: "Arduino",
+//   },
+//   {
+//     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/raspberrypi/raspberrypi-original.svg",
+//     name: "Raspberry Pi",
+//   },
+//   {
+//     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/embeddedc/embeddedc-original.svg",
+//     name: "Embedded C",
+//   },
+//   {
+//     img: "https://www.vectorlogo.zone/logos/arm/arm-icon.svg",
+//     name: "ARM Cortex",
+//   },
+//   {
+//     img: "https://cdn.worldvectorlogo.com/logos/esp32.svg",
+//     name: "ESP32",
+//   },
+//   {
+//     img: "https://www.vectorlogo.zone/logos/st_com/st_com-icon.svg",
+//     name: "STM32",
+//   },
+//   {
+//     img: "https://www.vectorlogo.zone/logos/freertos/freertos-icon.svg",
+//     name: "FreeRTOS",
+//   },
+//   {
+//     img: "https://cdn.worldvectorlogo.com/logos/altium-designer-1.svg",
+//     name: "Altium Designer",
+//   },
+//   {
+//     img: "https://www.vectorlogo.zone/logos/kicad/kicad-icon.svg",
+//     name: "KiCad",
+//   },
+//   {
+//     img: "https://cdn.worldvectorlogo.com/logos/eagle-7.svg",
+//     name: "Eagle PCB",
+//   },
+//   {
+//     img: "https://www.vectorlogo.zone/logos/autodesk/autodesk-icon.svg",
+//     name: "Fusion 360",
+//   },
+//   {
+//     img: "https://www.vectorlogo.zone/logos/solidworks/solidworks-icon.svg",
+//     name: "SolidWorks",
+//   },
+//   {
+//     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/verilog/verilog-original.svg",
+//     name: "Verilog",
+//   },
+//   {
+//     img: "https://www.vectorlogo.zone/logos/xilinx/xilinx-icon.svg",
+//     name: "FPGA/Xilinx",
+//   },
+// ];
+
 export const stackList = [
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/flutter/flutter-original.svg",
+    name: "Flutter",
+  },
   {
     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg",
     name: "HTML",
@@ -185,11 +372,31 @@ export const stackList = [
     name: "OpenCV",
   },
   {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/pytorch/pytorch-original.svg",
+    name: "PyTorch",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/scikitlearn/scikitlearn-original.svg",
+    name: "Scikit-learn",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/pandas/pandas-original.svg",
+    name: "Pandas",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/numpy/numpy-original.svg",
+    name: "NumPy",
+  },
+  {
     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/arduino/arduino-original.svg",
     name: "Arduino",
   },
   {
     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/raspberrypi/raspberrypi-original.svg",
     name: "Raspberry Pi",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/embeddedc/embeddedc-original.svg",
+    name: "Embedded C",
   },
 ];
